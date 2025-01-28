930c0c34c1972cf75992bfc5bfb50cd77f1a862e
Miara Satu <miarasatu@gmail.com>
2025-01-28 09:38:42 +0300

    build the login form

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -10,24 +10,56 @@ const Login = () => {
   const submitFormHandler = async (e) => {
     e.preventDefault()
     const response = await loginService.apiLogin({username: email, password: password})
-    if(response) {
+    if(response) { 
       login(response.token, response.user)
     }
   }
 
-  return <div>
-    Please check login before enter here!!!!
-    <form className="bock" onSubmit={submitFormHandler}>
-      <div>
-        <label htmlFor="f-email">email</label>
-        <input type="text" name="email" id="f-email" value={email} onChange={(e) => {setEmail(e.target.value)}} />
+  return <div className="w-full h-screen flex items-center bg-gray-700">
+    <div className="w-1/4 mx-auto">
+      <div className="w-full pt-8 bg-gray-800 shadow-md">
+        <div className="text-center mb-4 px-8">
+          <div className="font-semibold text-lg text-gray-100">
+            Welcome to the web chat app!
+          </div>
+          <div className="text-sm text-gray-300">
+            You can communicate with all your friends here! just send her an invitation and you can do what you want with her!
+          </div>
+        </div>
+        <form className="bock" onSubmit={submitFormHandler}>
+          <div className="w-full px-8 py-3 shadow-inner shadow-gray-900">
+            <input 
+              className="w-full border-none outline-none bg-transparent text-gray-300 placeholder:text-gray-300"
+              type="text" 
+              name="email" 
+              id="f-email" 
+              placeholder="email" 
+              value={email} 
+              onChange={(e) => {setEmail(e.target.value)}} 
+            />
+          </div>
+          <div className="w-full px-8 py-3 shadow-inner shadow-gray-900">
+            <input 
+              className="w-full border-none outline-none bg-transparent text-gray-300 placeholder:text-gray-300"
+              type="password" 
+              name="password" 
+              placeholder="password" 
+              id="f-password" 
+              value={password} 
+              onChange={(e) => {setPassword(e.target.value)}} 
+            />
+          </div>
+          <button 
+            className="w-full py-3 bg-gray-900 text-gray-100"
+            type="submit"
+          >Login</button>
+        </form>
       </div>
-      <div>
-        <label htmlFor="f-password">password</label>
-        <input type="password" name="password" id="f-password" value={password} onChange={(e) => {setPassword(e.target.value)}} />
+      <div className="flex justify-between mt-2 text-sm text-gray-200">
+        <div>Forgot password</div>
+        <div>Sign up</div>
       </div>
-      <button type="submit">Login</button>
-    </form>
+    </div>
   </div>
 }
 
