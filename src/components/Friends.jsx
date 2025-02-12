f720f553007751d5ed6e29185683eefe94f568b5
Miara Satu <miarasatu@gmail.com>
2025-02-12 12:38:26 +0300

    make Invitations block to be scrollable

--- src/components/Friends.jsx
+++ src/components/Friends.jsx
@@ -5,7 +5,7 @@ import Invitations from "./friends/Invitations"
 const Friends = () => {
   return <FriendsContextProvider>
     <div className="w-full h-full flex">
-      <div className="w-3/4 px-8 py-4 bg-gray-50">
+      <div className="w-3/4 h-screen overflow-scroll px-8 py-4 bg-gray-50">
         <Invitations />
       </div>
       <div className="w-1/4 pr-8 pl-4 py-4">
